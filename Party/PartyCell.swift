b589e871295aae03666f31008a01b756693e3b39
lokeshreddy <lokeshreddy@lokeshreddys-Mac.local>
2017-01-01 22:14:15 -0500

    first commit

---
+++ Party/PartyCell.swift
@@ -0,0 +1,24 @@
+//
+//  PartyCell.swift
+//  Party
+//
+//  Created by lokeshreddy on 1/1/17.
+//  Copyright © 2017 lokeshreddy. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Party/PartyRock.swift
@@ -0,0 +1,9 @@
+//
+//  PartyRock.swift
+//  Party
+//
+//  Created by lokeshreddy on 1/1/17.
+//  Copyright © 2017 lokeshreddy. All rights reserved.
+//
+
+import Foundation
